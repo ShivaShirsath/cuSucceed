361db97f00664aa275c4d8becdf45c68636d059c
Shiva Shirsath <Shiva.S.Shirsath@gmail.com>
2023-01-27 22:25:01 +0530

    🦕

--- README.md
+++ README.md
@@ -6,3 +6,19 @@
     ⋆
   </a>
 </h1>
+
+<!--
+
+<h1>Hello</h1>
+<style>
+  h1{
+    color:red;
+  }
+</style>
+<script>
+document.write(`
+<marquee>Hello</marquee>
+`);
+</script>
+
+-->
--- src/App.js
+++ src/App.js
@@ -1,61 +1,50 @@
 import React from "react";
-import Account from "./components/Account";
 import { Route, Routes } from "react-router-dom";
-import { AuthContextProvider } from "./context/AuthContext";
-import ProtectedRoute from "./components/ProtectedRoute";
 import { Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Home from "./pages/Home";
-import EditorPage from "./pages/EditorPage";
 
 function App() {
-	return (
-		<>
-			<Toaster
-				position="top-center"
-				reverseOrder={false}
-				gutter={2}
-				toastOptions={{
-					icon: "✅",
-					style: {
-						borderRadius: "1.5vmin",
-						background: "hsla(0, 0%, 0%, 0.5)",
-						backdropFilter: "blur(.25vmin)",
-						color: "#fff",
-					},
-				}}
-			/>
-			<AuthContextProvider>
-				<Routes>
-					<Route
-						path="/"
-						element={
-							<ProtectedRoute>
-								<Home />
-							</ProtectedRoute>
-						}
-					/>
-					<Route
-						path="/account"
-						element={
-							<ProtectedRoute>
-								<Account />
-							</ProtectedRoute>
-						}
-					/>
-					<Route
-						path="/editor"
-						element={
-							<ProtectedRoute>
-								<EditorPage />
-							</ProtectedRoute>
-						}
-					/>
-					<Route path="*" element={<Navigate to="/" />} />
-				</Routes>
-			</AuthContextProvider>
-		</>
-	);
+  return (
+    <>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        gutter={2}
+        toastOptions={{
+          icon: "✅",
+          style: {
+            borderRadius: "1.5vmin",
+            background: "hsla(0, 0%, 0%, 0.5)",
+            backdropFilter: "blur(.25vmin)",
+            color: "#fff",
+          },
+        }}
+      />
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="##">Skills</a>
+          </li>
+          <li>
+            <a href="###">Contact</a>
+          </li>
+          <li>
+            <a href="####">About</a>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/xyz" element={<>xyz</>} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+      <footer></footer>
+    </>
+  );
 }
 
 export default App;
